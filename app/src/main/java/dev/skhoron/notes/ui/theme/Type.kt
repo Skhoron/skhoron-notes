b1590dcadc7461414b376da02682ce4d73b0539f
Skhoron <306775767+Skhoron@users.noreply.github.com>
2026-07-28 05:44:53 +0500

    Create Type.kt

---
+++ app/src/main/java/dev/skhoron/notes/ui/theme/Type.kt
@@ -0,0 +1,15 @@
+package dev.skhoron.notes.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val SkhoronTypography = Typography(
+    titleLarge = TextStyle(fontWeight = FontWeight.Bold, fontSize = 20.sp),
+    titleMedium = TextStyle(fontWeight = FontWeight.SemiBold, fontSize = 15.sp),
+    bodyLarge = TextStyle(fontWeight = FontWeight.Normal, fontSize = 16.sp),
+    bodyMedium = TextStyle(fontWeight = FontWeight.Normal, fontSize = 14.sp),
+    bodySmall = TextStyle(fontWeight = FontWeight.Normal, fontSize = 12.sp),
+    labelSmall = TextStyle(fontWeight = FontWeight.Medium, fontSize = 11.sp),
+)
