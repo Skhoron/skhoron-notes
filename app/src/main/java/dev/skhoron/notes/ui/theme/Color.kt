3fca9e103d5cd2eeb42045a679b82670da40f1c6
Skhoron <306775767+Skhoron@users.noreply.github.com>
2026-07-28 13:50:31 +0500

    Create Color.kt

---
+++ app/src/main/java/dev/skhoron/notes/ui/theme/Color.kt
@@ -0,0 +1,41 @@
+package dev.skhoron.notes.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BgDark = Color(0xFF0E0E0E)
+val SurfaceDark = Color(0xFF161616)
+val Surface2Dark = Color(0xFF1E1E1E)
+val BorderDark = Color(0xFF2A2A2A)
+
+val BgLight = Color(0xFFF5F5F5)
+val SurfaceLight = Color(0xFFFFFFFF)
+val Surface2Light = Color(0xFFFAFAFA)
+val BorderLight = Color(0xFFE2E2E2)
+
+val TextDark = Color(0xFFF5F5F5)
+val TextDimDark = Color(0xFF9E9E9E)
+val TextFaintDark = Color(0xFF5C5C5C)
+
+val TextLight = Color(0xFF161616)
+val TextDimLight = Color(0xFF6B6B6B)
+val TextFaintLight = Color(0xFFB0B0B0)
+
+val AccentDefault = Color(0xFF16C172)
+
+val NoteColorPalette = listOf(
+    null,
+    Color(0xFF16C172),
+    Color(0xFF3B82F6),
+    Color(0xFFA855F7),
+    Color(0xFFEF4444),
+    Color(0xFFF59E0B),
+)
+
+val AccentPalette = listOf(
+    Color(0xFF16C172),
+    Color(0xFF3B82F6),
+    Color(0xFFA855F7),
+    Color(0xFFEF4444),
+    Color(0xFFF59E0B),
+    Color(0xFF14B8A6),
+)
